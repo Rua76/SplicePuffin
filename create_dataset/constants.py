c89c3b2c9c49234c5686cbc84a40658bdf0a7d94
Rua76 <yhsz5566@uchicago.edu>
2025-10-20 10:41:01 -0500

    remove old files

--- create_dataset/constants.py
+++
@@ -1,8 +0,0 @@
-CL_max=1000
-# Maximum nucleotide context length (CL_max/2 on either side of the 
-# position of interest)
-# CL_max should be an even number
-
-SL=4000
-# Sequence length of SpliceAIs (SL+CL will be the input length and
-# SL will be the output length)
